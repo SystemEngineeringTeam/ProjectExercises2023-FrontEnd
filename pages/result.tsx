a318a1cf3c15f5536086273bbcb09ff8bc2877a3
nanairo2743 <natsu0704.nn@au.com>
2023-10-21 13:45:24 +0900

    画像表示した

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,9 +1,38 @@
-
+import { Inter } from "next/font/google";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
+import { Center, Square, Circle } from "@chakra-ui/react";
+import Router from "next/router";
+import Image from "next/image";
 
 export default function Result() {
-    return (
-        <main>
-            hoge
-        </main>
-    )
+  const handler = (path: string) => {
+    Router.push(path);
+  };
+
+  return (
+    <main>
+      <Stack h="100vh" align="center" justify="center" spacing={120}>
+        <Box>
+          <Image
+            src="https://img.esa.io/uploads/production/attachments/13979/2023/10/03/129607/614cedd5-e28b-4677-b313-3588a38b0c80.png"
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
+        </Box>
+        <Box>
+          <Button colorScheme="blue" onClick={() => handler("/")}>
+            home
+          </Button>
+        </Box>
+      </Stack>
+    </main>
+  );
 }
