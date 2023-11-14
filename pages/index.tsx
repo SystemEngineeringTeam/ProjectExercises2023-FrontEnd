5aec4ef3e34d85142fd7220c758c98b70d59a40a
nanairo2743 <natsu0704.nn@au.com>
2023-11-14 14:49:45 +0900

    add: axiosの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,26 @@
-import { Button } from '@chakra-ui/react'
-import { Center } from '@chakra-ui/react'
-import Router from 'next/router'
-import { UserInformation } from '../components/userInformation'
-import { Http } from '../components/http'
+import { Button } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
+import Router from "next/router";
+import { UserInformation } from "../components/userInformation";
+import { Http } from "../components/http";
+import axios, { AxiosResponse } from "axios";
+
+type BoardSurface = {
+  StartTime: string;
+  FinishTime: string;
+  url: string;
+};
 
 export default function Home() {
-
-  const handler = (path: string) => {
-    Router.push(path)
-  }
+  const handler = async (path: string) => {
+    //TODO グラフの画像を受け取る
+    const response: AxiosResponse<BoardSurface[]> = await axios.post(
+      "https://heartbeat.sysken.net/api/v1/finish"
+    );
+    console.log(response.data);
+    //URL遷移
+    Router.push(path);
+  };
 
   return (
     <main>
@@ -17,11 +29,18 @@ export default function Home() {
       <UserInformation face="😏" position="西" pulse="90"></UserInformation>
       <UserInformation face="😇" position="南" pulse="0"></UserInformation>
 
-      <Center h='100vh' color='white'>
-        <Button height={12} colorScheme='cyan' paddingX='48px' fontSize="24" borderRadius={16} onClick={() => handler('/result')}>
+      <Center h="100vh" color="white">
+        <Button
+          height={12}
+          colorScheme="cyan"
+          paddingX="48px"
+          fontSize="24"
+          borderRadius={16}
+          onClick={() => handler("/result")}
+        >
           終了
         </Button>
       </Center>
     </main>
-  )
+  );
 }
